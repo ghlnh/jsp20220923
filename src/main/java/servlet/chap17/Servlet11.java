aa7848505acb2974feb6305f23ac71dbb5dfc875
ghlnh <lnh404gom@gmail.com>
2022-10-11 17:50:14 +0900

    20221011 학습내용

--- src/main/java/servlet/chap17/Servlet11.java
+++ src/main/java/servlet/chap17/Servlet11.java
@@ -16,7 +16,7 @@ import jsp20220923.chap07.Book;
 /**
  * Servlet implementation class Servlet10
  */
-@WebServlet("/Servlet11")
+@WebServlet(value = {"/Servlet11", "/filter/servlet11"})
 public class Servlet11 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
